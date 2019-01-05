8c633194a68ee27c78e50dd7c74860f9fb988b80
makoslayer13 <taquintana07@gmail.com>
2019-01-05 11:45:55 -0800

    Add files via upload

---
+++ whitespace.py
@@ -0,0 +1,10 @@
+#\n will add a new line to the string after
+#\t will add a new tab to the string
+print("Languages:\nPython\nC\nJavascript")
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+
+#Whitespace can be used as such
+
+favorite_language = ' Python '
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
